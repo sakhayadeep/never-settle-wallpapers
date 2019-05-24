5e9c3ace3502c83dbf0eda9cab68cda7eac154fc
Swamim Saikia <swamimsaikia18@gmail.com>
2019-05-24 23:57:59 +0530

    working list view

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,53 @@ import './wallpaper_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final String _appTitle = "Never Settle";
+  bool darkThemeEnabled = false;
+
   @override
   Widget build(context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: darkThemeEnabled ? Brightness.dark : Brightness.light,
         primarySwatch: Colors.deepOrange,
       ),
       home: Scaffold(
+          appBar: AppBar(
+            title: Text(_appTitle),
+          ),
           drawer: Drawer(
             child: ListView(
               padding: EdgeInsets.all(0),
               children: <Widget>[
                 DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.orange),
                   child: ListTile(
                     title: Text("Sakhayadeep Nath"),
                     subtitle: Text("sakhayadeepnath@gmaill.com"),
                     trailing: Icon(Icons.account_circle),
+                    onTap: () {},
+                  ),
+                ),
+                ListTile(
+                  title: Text("Dark Theme"),
+                  trailing: Switch(
+                    value: darkThemeEnabled,
+                    onChanged: (changedTheme) {
+                      darkThemeEnabled = changedTheme;
+                      setState(() {});
+                    },
                   ),
                 )
               ],
             ),
-          ),
-          appBar: AppBar(
-            title: Text("Never Settle"),
           ),
           body: WallpaperManager("Coolfie")),
     );
